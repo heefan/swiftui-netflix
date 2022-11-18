94dca3be67ea3a490659deba3453f8fe1ec15ddd
LI Tian <li_tian@singaporeair.com.sg>
2022-11-18 17:09:35 +0800

    feat: create project and add kingfisher swiftpackage

---
+++ HFNetflix/HFNetflix/HFNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  HFNetflixApp.swift
+//  HFNetflix
+//
+//  Created by LI Tian on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HFNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HFNetflix/HFNetflix/HomePage/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  HFNetflix
+//
+//  Created by LI Tian on 18/11/22.
+//
+
+import Foundation
